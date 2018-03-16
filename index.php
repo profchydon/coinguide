ff1a8e677634ecc651482d6ba0e15a34d226ce5b
CHIDI-NKWOCHA\profchydon <profchydon@gmail.com>
2018-03-16 15:56:48 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,71 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Popular Cryptocurrencies</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <!-- <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/script.js"></script> -->
+</head>
+
+<body>
+    <div id="banner">
+        <h2 class="banner-h2"> Coin Guide</h2>
+    </div>
+    <h2 id="heading">Poloniex Cryptocurrencies Trade Guide</h2>
+    <!-- <h5>Sorted by popularity, in descending order</h5> -->
+    <div class="container">
+
+      <div class="row">
+
+          <div class="col-md-12">
+             <h4>Top Gaining Cryptocurrencies</h4>
+             <table class="table table-striped table-responsive">
+                 <thead>
+                     <tr>
+                         <th>S/no</th>
+                         <th>Coin</th>
+                         <th>Currency Pair</th>
+                         <th>Buy trade Vol. 5mins ago</th>
+                         <th>Total buy trade vol. 5mins ago</th>
+                         <th>% of coin in total buy trade vol. 5mins ago</th>
+                         <th>Current Buy trade Vol.</th>
+                         <th>Current Total buy trade volume</th>
+                         <th>% of coin in total buy trade volume</th>
+
+                     </tr>
+                 </thead>
+                 <tbody>
+
+
+
+               </tbody>
+               </table>
+
+          </div>
+
+
+          <div class="col-md-12">
+              <div class="alert alert-success" role="alert">
+                  <h4>Who is gaining the shift?</h4>
+
+
+
+                  </p>
+              </div>
+          </div>
+
+      </div>
+
+
+
+    </div>
+
+</body></html>
